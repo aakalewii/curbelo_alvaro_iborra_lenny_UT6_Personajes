156b9e0e33af488f343053ce359bf17557003775
alvaro123124 <alvarobudy12@gmail.com>
2025-04-24 16:18:33 +0100

    main

--- src/com/app/juego.java
+++ src/com/app/juego.java
@@ -7,22 +7,7 @@ import com.model.hechicero;
 import com.model.asesino;
 
 public class juego {
-    public static void main(String[] args) {
-        ArrayList<Personaje> personajes = new ArrayList<>();
-
-        // Agregar personajes a la lista
-        personajes.add(new guerrero("Guts", 10, 100, 50, 30, 20));
-        personajes.add(new mago("Morgana", 12, 80, 100, 70));
-        personajes.add(new arquero("Dibu", 8, 90, 60, 30));
-        personajes.add(new hechicero("Jimmy", 15, 70, 120, 80));
-        personajes.add(new asesino("Alcapone", 9, 85, 40, 50));
-
-        // Mostrar los personajes
-        for (Personaje personaje : personajes) {
-            System.out.println(personaje);
-        }
-
-        void mostrarAcciones(){
+    public static void mostrarAcciones(ArrayList<Personaje> personajes) {
             for (Personaje personaje : personajes) {
                 if (personaje instanceof guerrero) {
                     ((guerrero) personaje).defender();
@@ -31,11 +16,10 @@ public class juego {
                 } else if (personaje instanceof arquero) {
                     ((arquero) personaje).moverse();
                 } else if (personaje instanceof hechicero) {
-                    ((hechicero) personaje).sigilo();
+                    ((hechicero) personaje).defender();
                 } else if (personaje instanceof asesino) {
-                    ((asesino) personaje).atacar();
+                    ((asesino) personaje).sigilo();
                 }
             }
         }
     }
-}
--- src/com/app/main.java
+++ src/com/app/main.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import com.model.Personaje;
+import com.model.guerrero;
+import com.model.mago;
+import com.model.arquero;
+import com.model.hechicero;
+import com.model.asesino;
+
+
 public class main {
-    //jscd
+    public static void main(String[] args) {
+        ArrayList<Personaje> personajes = new ArrayList<>();
+        personajes.add(new guerrero("Guts", 10, 100, 50, 30, 20));
+        personajes.add(new mago("Morgana", 12, 80, 40, 20));
+        personajes.add(new arquero("Dibu", 8, 90, 30, 25));
+        personajes.add(new hechicero("Jimmy", 10, 80, 20, 15));
+        personajes.add(new asesino("Alcapone", 9, 80, 25, 10));
+
+        juego.mostrarAcciones(personajes);
+    }
 }
