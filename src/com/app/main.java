66a01b753b5ac872dee8375db4d2be0ed73889a3
aakalewii <146139742+aakalewii@users.noreply.github.com>
2025-04-29 19:26:41 +0100

    main corregido

--- src/com/app/main.java
+++ src/com/app/main.java
@@ -1,20 +1,13 @@
 import java.util.ArrayList;
-import com.model.Personaje;
-import com.model.guerrero;
-import com.model.mago;
-import com.model.arquero;
-import com.model.hechicero;
-import com.model.asesino;
-
 
 public class main {
     public static void main(String[] args) {
         ArrayList<Personaje> personajes = new ArrayList<>();
-        personajes.add(new guerrero("Guts", 10, 100, 50, 30, 20));
-        personajes.add(new mago("Morgana", 12, 80, 40, 20));
-        personajes.add(new arquero("Dibu", 8, 90, 30, 25));
-        personajes.add(new hechicero("Jimmy", 10, 80, 20, 15));
-        personajes.add(new asesino("Alcapone", 9, 80, 25, 10));
+        personajes.add(new Guerrero("Guts", 10, 100, 50, 30, 20));
+        personajes.add(new Mago("Morgana", 12, 80, 40, 20));
+        personajes.add(new Arquero("Dibu", 8, 90, 30, 25));
+        personajes.add(new Hechicero("Jimmy", 10, 80, 20, 15));
+        personajes.add(new Asesino("Alcapone", 9, 80, 25, 10));
 
         juego.mostrarAcciones(personajes);
     }
